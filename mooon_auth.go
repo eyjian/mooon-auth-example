0ff08dbe18282bdb4c5da44c47acd43decba2e42
jayyi <eyjian@qq.com>
2024-02-03 11:07:23 +0800

    Update mooon_auth.go

--- mooon_auth.go
+++ mooon_auth.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+    "flag"
+    "fmt"
 
-	"mooon-auth-example/internal/config"
-	"mooon-auth-example/internal/server"
-	"mooon-auth-example/internal/svc"
-	"mooon-auth-example/pb/mooon_auth"
+    "mooon-auth-example/internal/config"
+    "mooon-auth-example/internal/server"
+    "mooon-auth-example/internal/svc"
+    "mooon-auth-example/pb/mooon_auth"
 
-	"github.com/zeromicro/go-zero/core/conf"
-	"github.com/zeromicro/go-zero/core/service"
-	"github.com/zeromicro/go-zero/zrpc"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+    "github.com/zeromicro/go-zero/core/conf"
+    "github.com/zeromicro/go-zero/core/service"
+    "github.com/zeromicro/go-zero/zrpc"
+    "google.golang.org/grpc"
+    "google.golang.org/grpc/reflection"
 )
 
 var configFile = flag.String("f", "etc/mooon_auth.yaml", "the config file")
 
 func main() {
-	flag.Parse()
+    flag.Parse()
 
-	var c config.Config
-	conf.MustLoad(*configFile, &c)
-	ctx := svc.NewServiceContext(c)
+    var c config.Config
+    conf.MustLoad(*configFile, &c)
+    ctx := svc.NewServiceContext(c)
 
-	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
-		mooon_auth.RegisterMooonAuthServer(grpcServer, server.NewMooonAuthServer(ctx))
+    s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
+        mooon_auth.RegisterMooonAuthServer(grpcServer, server.NewMooonAuthServer(ctx))
 
-		if c.Mode == service.DevMode || c.Mode == service.TestMode {
-			reflection.Register(grpcServer)
-		}
-	})
-	defer s.Stop()
+        if c.Mode == service.DevMode || c.Mode == service.TestMode {
+            reflection.Register(grpcServer)
+        }
+    })
+    defer s.Stop()
 
-	fmt.Printf("Starting rpc server at %s...\n", c.ListenOn)
-	s.Start()
+    fmt.Printf("Starting auth server at %s...\n", c.ListenOn)
+    s.Start()
 }
